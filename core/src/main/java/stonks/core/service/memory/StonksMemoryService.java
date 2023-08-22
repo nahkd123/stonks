c607c21b8c6a966291df9c656d650089a768d410
nahkd123 <nahkd123@gmail.com>
2023-08-23 00:02:40 +0700

    Add missing break to instantOffer() method in StonksMemoryService

--- core/src/main/java/stonks/core/service/memory/StonksMemoryService.java
+++ core/src/main/java/stonks/core/service/memory/StonksMemoryService.java
@@ -203,8 +203,10 @@ public class StonksMemoryService implements LocalStonksService {
 		switch (type) {
 		case BUY:
 			exec.executeInstantBuy(productEntry.sellOffers.iterator(), offerFilledEvents::emit);
+			break;
 		case SELL:
 			exec.executeInstantSell(productEntry.buyOffers.iterator(), offerFilledEvents::emit);
+			break;
 		}
 
 		return Task.resolved(new InstantOfferExecuteResult(exec.getCurrentUnits(), exec.getCurrentBalance()));
